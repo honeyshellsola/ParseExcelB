4f64f43f16cdfad83589610643a3b8c601bf240d
honeyshellsola <honey.kumar@shell.com>
2023-06-20 18:55:06 +0530

    save to another workbook

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ public class Program
     public static void Main()
     {
         // Path to the XLSB file
-        string filePath = @"C:\Users\workspace\xlsbfile.xlsb";
+        string filePath = @"C:\Users\Honey.Kumar\OneDrive - Shell\Documents\Honey Kumar\workspace\xlsbfile.xlsb";
 
         // Load the XLSB file using Spire.XLS
         Workbook workbook = new Workbook();
@@ -15,28 +15,37 @@ public class Program
 
         // Get the "Blank 3-U" worksheet
         Worksheet worksheet = workbook.Worksheets["Blank 3-U"];
-        DataTable dt = worksheet.ExportDataTable(worksheet.AllocatedRange, false, true);
-        int count= 0;
-        foreach (DataRow row in dt.Rows)
-        {
-            count++;
-            if (count == 6)
-            {
-            // Loop through each column in the DataRow
-                foreach (DataColumn column in dt.Columns)
-                {
-                    // Access the cell value using the column name or index
-                    object cellValue = row[column];
 
-                    // Print the cell value on the console
-                    Console.WriteLine($"Column: {column.ColumnName}, Value: {cellValue}");
-                }
-            }
-        }
+        Workbook outputWorkbook = new Workbook();
+
+
+
+        DataTable dt = worksheet.ExportDataTable(worksheet.AllocatedRange, false, true);
+        Worksheet outputWorksheet = outputWorkbook.Worksheets[0];
+        outputWorksheet.InsertDataTable(dt, true, 1, 1);
+        outputWorkbook.SaveToFile(@"C:\Users\Honey.Kumar\OneDrive - Shell\Documents\Honey Kumar\workspace\blank3u.xlsx", FileFormat.Version2013);
+        // int count= 0;
+        // foreach (DataRow row in dt.Rows)
+        // {
+        //     count++;
+        //     if (count == 6)
+        //     {
+        //     // Loop through each column in the DataRow
+        //         foreach (DataColumn column in dt.Columns)
+        //         {
+        //             // Access the cell value using the column name or index
+        //             object cellValue = row[column];
+
+        //             // Print the cell value on the console
+        //             Console.WriteLine($"Column: {column.ColumnName}, Value: {cellValue}");
+        //         }
+        //     }
+        // }
 
      
 
         // Dispose the workbook
         workbook.Dispose();
+        outputWorkbook.Dispose();
     }
 }
